1a2287280a72a112bd5b380ce37a2fa20172b44f
kvfedot4 <kvfedot4@stream.ru>
2021-07-07 21:09:45 +0300

    added close driver

--- src/test/java/tests/TestBase.java
+++ src/test/java/tests/TestBase.java
@@ -1,7 +1,10 @@
 package tests;
 
 import com.codeborne.selenide.Configuration;
+import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeAll;
+
+import static com.codeborne.selenide.WebDriverRunner.closeWebDriver;
 
 public class TestBase {
     @BeforeAll
@@ -9,4 +12,9 @@ public class TestBase {
         Configuration.startMaximized = true;
         Configuration.remote = "https://user1:1234@selenoid.autotests.cloud/wd/hub/";
     }
-}
+        @AfterEach
+        public void tearDown() {
+            closeWebDriver();
+        }
+    }
+
