7458af64a68c764ef4c97461b0635556b67a49ec
kvfedot4 <kvfedot4@stream.ru>
2021-07-07 21:27:14 +0300

    added allure attachment

--- src/test/java/tests/TestBase.java
+++ src/test/java/tests/TestBase.java
@@ -1,6 +1,9 @@
 package tests;
 
 import com.codeborne.selenide.Configuration;
+import com.codeborne.selenide.logevents.SelenideLogger;
+import helpers.Attach;
+import io.qameta.allure.selenide.AllureSelenide;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeAll;
 
@@ -9,11 +12,15 @@ import static com.codeborne.selenide.WebDriverRunner.closeWebDriver;
 public class TestBase {
     @BeforeAll
     static void setup() {
+        SelenideLogger.addListener("AllureSelenide",new AllureSelenide());
         Configuration.startMaximized = true;
         Configuration.remote = "https://user1:1234@selenoid.autotests.cloud/wd/hub/";
     }
         @AfterEach
         public void tearDown() {
+            Attach.screenshotAs("Last screenshot");
+            Attach.pageSource();
+            Attach.browserConsoleLogs();
             closeWebDriver();
         }
     }
