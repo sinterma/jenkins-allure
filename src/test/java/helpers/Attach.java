401c436bdc2294442191b34001b906b5a132ddb6
kvfedot4 <kvfedot4@stream.ru>
2021-07-07 22:04:54 +0300

    added  video attachment

--- src/test/java/helpers/Attach.java
+++ src/test/java/helpers/Attach.java
@@ -1,3 +1,4 @@
+
 package helpers;
 
 import com.codeborne.selenide.Selenide;
@@ -5,10 +6,16 @@ import io.qameta.allure.Allure;
 import io.qameta.allure.Attachment;
 import org.openqa.selenium.OutputType;
 import org.openqa.selenium.TakesScreenshot;
+import org.openqa.selenium.remote.RemoteWebDriver;
 
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.io.InputStream;
+import java.net.MalformedURLException;
+import java.net.URL;
 import java.nio.charset.StandardCharsets;
 
-
+import static com.codeborne.selenide.Selenide.sleep;
 import static com.codeborne.selenide.WebDriverRunner.getWebDriver;
 import static org.openqa.selenium.logging.LogType.BROWSER;
 
@@ -34,4 +41,37 @@ public class Attach {
                 String.join("\n", Selenide.getWebDriverLogs(BROWSER))
         );
     }
+
+    public static void addVideo(String sessionId) {
+        URL videoUrl = getVideoUrl(sessionId);
+        if (videoUrl != null) {
+            InputStream videoInputStream = null;
+            sleep(1000);
+
+            for (int i = 0; i < 10; i++) {
+                try {
+                    videoInputStream = videoUrl.openStream();
+                    break;
+                } catch (FileNotFoundException e) {
+                    sleep(1000);
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+            Allure.addAttachment("Video", "video/mp4", videoInputStream, "mp4");
+        }
+    }
+
+    public static URL getVideoUrl(String sessionId) {
+        String videoUrl = "https://selenoid.autotests.cloud/video/" + sessionId + ".mp4";
+
+        try {
+            return new URL(videoUrl);
+        } catch (MalformedURLException e) {
+            e.printStackTrace();
+        }
+        return null;
+    }
+
 }
+
