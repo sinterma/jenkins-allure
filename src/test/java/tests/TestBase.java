c18df7ebb0d1e1daeae671722837c19e9c0882de
kvfedot4 <kvfedot4@stream.ru>
2021-07-07 21:01:40 +0300

    added selenoid support

--- src/test/java/tests/TestBase.java
+++ src/test/java/tests/TestBase.java
@@ -7,5 +7,6 @@ public class TestBase {
     @BeforeAll
     static void setup() {
         Configuration.startMaximized = true;
+        Configuration.remote = "https://user1:1234@selenoid.autotests.cloud/wd/hub/";
     }
 }
