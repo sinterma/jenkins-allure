f11eabfe71679cd67a4a798324a469b842acde59
kvfedot4 <kvfedot4@stream.ru>
2021-07-13 19:37:44 +0300

    added  some test for Telegram

---
+++ src/test/java/new_tests/TelegramTests3.java
@@ -0,0 +1,47 @@
+package new_tests;
+
+import org.junit.jupiter.api.Disabled;
+import org.junit.jupiter.api.Tag;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+@Tag("fake")
+
+public class TelegramTests3 {
+
+    @Test
+    @Disabled
+    void some1Test() {
+        assertTrue(false);
+    }
+
+    @Test
+    @Disabled
+    void some2Test() {
+        assertTrue(false);
+    }
+
+    @Test
+    @Disabled
+    void some3Test() {
+        assertTrue(false);
+    }
+
+    @Test
+    @Disabled
+    void some4Test() {
+        assertTrue(false);
+    }
+
+    @Test
+    @Disabled
+    void some5Test() {
+        assertTrue(false);
+    }
+    @Test
+    @Disabled
+    void some6Test() {
+        assertTrue(false);
+    }
+}
